cb4eff432480c4109b7786561cd22c6cd5f4337e
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-06-23 20:32:39 -0300

    cria interface de login e registro

---
+++ GBank/AppViews/Register/View/RegisterViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RegisterViewController.swift
+//  GBank
+//
+//  Created by Guilherme de Assis dos Santos on 23/06/21.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var cpfTextField: UITextField!
+    @IBOutlet weak var nomeTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func criarContaPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func logarPressed(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
