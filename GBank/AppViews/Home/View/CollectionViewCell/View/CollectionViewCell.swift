a11db2bfd37395601f81db428cc3c11ef754535a
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-06-30 22:18:53 -0300

    Corrige layout na home

---
+++ GBank/AppViews/Home/View/CollectionViewCell/View/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  GBank
+//
+//  Created by Guilherme de Assis dos Santos on 30/06/21.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
